f685a8bedc9cd8d3e318f1c162128e7b65a21622
yuta-nishi <ypmtk437.0209@gmail.com>
2023-02-14 22:30:03 +0900

    Add 162. Find Peak Element

---
+++ 162_find_peak_element.py
@@ -0,0 +1,18 @@
+# n=len(nums)
+# Time:O(log n)
+# Space:O(1)
+
+from typing import List
+
+
+class Solution(object):
+    def findPeakElement(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[mid + 1]:
+                right = mid
+            else:
+                left = mid + 1
+        return left
