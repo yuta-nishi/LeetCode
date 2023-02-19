201d0188600a6d29bc74f82f0a30a5fe6c239daa
yuta-nishi <ypmtk437.0209@gmail.com>
2023-02-20 01:40:53 +0900

    Update 35. Search Insert Position

---
+++ 35_search_insert_position.py
@@ -0,0 +1,20 @@
+# n=len(nums)
+# Time: O(log n)
+# Space: O(1)
+
+from typing import List
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                return mid
+        return right + 1
