b7e3d041cd3e77d60b1841381bfe377ae602f09a
yuta-nishi <ypmtk437.0209@gmail.com>
2023-02-24 12:52:38 +0900

    Add some solution

--- hash_table/242_valid_anagram.py
+++ hash_table/242_valid_anagram.py
@@ -1,8 +1,23 @@
-# n=len(nums)
+# n=len(s)
+# m=len(t)
 # Time: O(n)
-# Space: O(n)
+# Space: O(n+m)
 
-from collections import Counter
+from collections import Counter, defaultdict
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s_count = defaultdict(int)
+        t_count = defaultdict(int)
+        for i in range(len(s)):
+            s_count[s[i]] += 1
+            t_count[t[i]] += 1
+        if s_count == t_count:
+            return True
+        return False
 
 
 class Solution:
@@ -10,5 +25,18 @@ class Solution:
         s_count = Counter(s)
         t_count = Counter(t)
         if s_count == t_count:
+            return True
+        return False
+
+
+# Time: O(nlog(n))
+# Space: O(1)
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_sort = sorted(s)
+        t_sort = sorted(t)
+        if s_sort == t_sort:
             return True
         return False
