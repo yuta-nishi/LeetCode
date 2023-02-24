6ce7de0b5128f2a590f4c0732633e66c768eddd5
yuta-nishi <ypmtk437.0209@gmail.com>
2023-02-24 12:20:30 +0900

    Add

---
+++ hash_table/217_contains_duplicate.py
@@ -0,0 +1,16 @@
+# n=len(nums)
+# Time: O(n)
+# Space: O(n)
+
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hash_set = set()
+        for num in nums:
+            if num in hash_set:
+                return True
+            else:
+                hash_set.add(num)
+        return False
