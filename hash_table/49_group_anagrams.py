41b007a8d7a5e6826eecf37b553d3e613b95c0d6
yuta-nishi <ypmtk437.0209@gmail.com>
2023-02-24 16:37:54 +0900

    Add another solution

--- hash_table/49_group_anagrams.py
+++ hash_table/49_group_anagrams.py
@@ -1,7 +1,7 @@
 # n=len(strs)
 # m=len(string)
-# Time: O(n*m)
-# Space: O(n*m)
+# Time: O(nm)
+# Space: O(nm)
 
 from collections import defaultdict
 from typing import List
@@ -15,4 +15,16 @@ class Solution:
             for char in string:
                 char_count[ord(char) - ord("a")] += 1
             hash_table[tuple(char_count)].append(string)
+        return hash_table.values()
+
+
+# Time: O(nmlog(m))
+# Space: O(nm)
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash_table = defaultdict(list)
+        for string in strs:
+            hash_table[tuple(sorted(string))].append(string)
         return hash_table.values()
