4806b7c2af184eda12ad4b10a0f20e8fe6a84c52
yuta-nishi <ypmtk437.0209@gmail.com>
2023-02-25 21:03:14 +0900

    Add 36. Valid Sudoku

---
+++ hash_table/36_valid_sudoku.py
@@ -0,0 +1,23 @@
+# Time: O(1)
+# Space: O(1)
+
+from typing import List
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    continue
+                num = int(board[i][j])
+                box_idx = (i // 3 * 3) + (j // 3)
+                if num in rows[i] or num in cols[j] or num in boxes[box_idx]:
+                    return False
+                rows[i].add(num)
+                cols[j].add(num)
+                boxes[box_idx].add(num)
+        return True
