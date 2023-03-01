bd15f74ec455bee1a3c4dba88b1b070159039955
yuta-nishi <ypmtk437.0209@gmail.com>
2023-03-01 23:39:26 +0900

    Update 1. Two Sum

--- hash-table/1_two_sum.py
+++ hash-table/1_two_sum.py
@@ -1,15 +1,30 @@
 # n=len(nums)
 # Time: O(n)
-# Space: O(n)
+# Space: O(1)
 
 from typing import List
 
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hash_table = {}
+        hashmap = {}
         for i in range(len(nums)):
             complement = target - nums[i]
-            if complement in hash_table:
-                return hash_table[complement], i
-            hash_table[nums[i]] = i
+            if complement in hashmap:
+                return hashmap[complement], i
+            hashmap[nums[i]] = i
+
+
+# Time: O(n)
+# Space: O(n)
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return i, hashmap[complement]
