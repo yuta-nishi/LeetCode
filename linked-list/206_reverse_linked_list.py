f05f001144c72a80296bc9f54d6cf47b342192e1
yuta-nishi <ypmtk437.0209@gmail.com>
2023-03-08 00:14:59 +0900

    Add 206. Reverse Linked List

---
+++ linked-list/206_reverse_linked_list.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+# Time: O(n)
+# Space: O(1)
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return None
+        prev = None
+        cur = head
+        while cur:
+            post = cur.next
+            cur.next = prev
+            prev = cur
+            cur = post
+        return prev
+
+
+# Time: O(n)
+# Space: O(n)
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        new_head = head
+        if head.next:
+            new_head = self.reverseList(head.next)
+            head.next.next = head
+        head.next = None
+        return new_head
