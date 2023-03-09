e9baa684f215c1e96c5a1de3f381752c6f02dfb0
yuta-nishi <ypmtk437.0209@gmail.com>
2023-03-10 02:11:45 +0900

    Add 143. Reorder List

---
+++ linked-list/143_reorder_list.py
@@ -0,0 +1,36 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+# Time: O(n)
+# Space: O(1)
+
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow = head
+        fast = head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+        first = head
+        second = prev
+        while second:
+            tmp1 = first.next
+            tmp2 = second.next
+            first.next = second
+            second.next = tmp1
+            first = tmp1
+            second = tmp2
